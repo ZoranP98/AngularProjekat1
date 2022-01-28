c408daa0f5e17db6afbfea0ac42349e113a87fa0
ZoranP98 <zoranplakalovic998@gmail.com>
2022-01-28 16:30:48 +0100

    petak28

--- src/app/news-card/news-card.component.ts
+++ src/app/news-card/news-card.component.ts
@@ -12,4 +12,28 @@ export class NewsCardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  products: any[] = [
+        {
+          "Id": 1,
+          "PostDate": "December 15, 2021",
+          "CTitle": "Lorem ipsum dolor sit amet, consetetur",
+          "CText": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos…",          
+          "imageUrl": "../../assets/Images/shutterstock_1686003652-2.png"
+        },
+        {
+          "Id": 2,
+          "PostDate": "December 15, 2021",
+          "CTitle": "Lorem ipsum dolor sit amet, consetetur",
+          "CText": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos…",          
+          "imageUrl": "../../assets/Images/shutterstock_1686003652-2.png"
+        },{
+          "Id": 3,
+          "PostDate": "December 15, 2021",
+          "CTitle": "Lorem ipsum dolor sit amet, consetetur",
+          "CText": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos…",          
+          "imageUrl": "../../assets/Images/shutterstock_1686003652-2.png"
+        },
+      ]; 
+
+
 }
